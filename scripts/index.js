b648bbfc879a36aa162e56268990f729dcf477c7
davemflick <davemflick@yahoo.com>
2017-04-04 14:20:33 -0400

    Basic html, js and css file. Put in a flipping div with question and answer sides

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+console.log('Connected')
+
+const flashCard = document.getElementById("fcard");
+const cardContain = document.getElementById("fcCont");
+
+fcCont.addEventListener('click',function(){flashCard.classList.toggle("flipped"), console.log(flashCard.classList)} )  
+
